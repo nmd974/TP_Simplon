d5292c2a717fe34f3402ea4f69c4f2a96da66b62
nmd974 <jha.payet@gmail.com>
2020-10-07 11:23:54 +0400

    V8 + arrays

---
+++ arrays/script.js
@@ -0,0 +1,23 @@
+var array1 = [4,8,7,9,1,5,4,6];
+var array2 = [7,6,5,2,1,3,7,4];
+var arrayRendu = [];  
+var listItem = document.createElement('div');
+
+$(document).ready(function loading(params) {
+    for (let i = 0; i < array1.length; i++) {
+        const element1 = array1[i];
+        const element2 = array2[i];
+        arrayRendu.push(element1 + element2);  
+    }
+
+    arrayRendu.forEach(element => {
+        var divToAdd = document.createElement('DIV');
+        divToAdd.innerHTML = element;
+        console.log(divToAdd);
+        listItem.appendChild(divToAdd);
+        
+    });
+    listItem.className='container';
+    $('#container').append(listItem);
+})
+
